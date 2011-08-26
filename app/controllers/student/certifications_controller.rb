4b13508933ac561a62737a4ecd8d985a343242ca
upender <upender@jasita.com>
2011-08-26 16:53:25 +0530

    workspace.xml ignored from git

--- app/controllers/student/certifications_controller.rb
+++ app/controllers/student/certifications_controller.rb
@@ -2,4 +2,9 @@ class Student::CertificationsController < ApplicationController
   def index
     @certifications_owned = current_user.owned_certifications
   end
+
+  def assignments
+   @owned_certification = OwnedCertification.includes(:student_assignments).find(params[:id])
+  end
+
 end
